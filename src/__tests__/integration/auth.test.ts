a5a9e29d5cd72a5f6c3114e61efc44c6fedc88dd
Helder-programer <nicollashelder@gmail.com>
2023-09-20 11:58:10 -0300

    Configurando ambiente para teste 01

---
+++ src/__tests__/integration/auth.test.ts
@@ -0,0 +1,5 @@
+describe('Auth Tests', () => {
+    it('should return user data and token', () => {
+        expect(2).toBe(2);
+    });
+});
--- src/domain/IUser.ts
+++ src/domain/IUser.ts
@@ -1,4 +1,4 @@
-import {Prisma} from '@prisma/client';
+import { Prisma } from '@prisma/client';
 export interface IUser {
     user_id: string;
     name: string;
